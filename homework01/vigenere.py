0e7fb973d7a1e81bcfed6d9184230175d24e8ec1
h3ic <krelesser@yandex.ru>
2019-09-28 15:40:33 +0300

    encrypt_vigenere function added

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,6 +10,22 @@ def encrypt_vigenere(plaintext, keyword):
     'LXFOPVEFRNHR'
     """
     # PUT YOUR CODE HERE
+    ciphertext = ''
+    if len(plaintext) > len(keyword):
+        keyword += keyword*((len(plaintext) // len(keyword))-1)
+        keyword += keyword[:(len(plaintext) % len(keyword))]
+    i = 0
+    for ch in plaintext:
+        if keyword[i].isupper():
+            shift = ord(keyword[i])-65
+        else:
+            shift = ord(keyword[i])-97
+        if ch.isupper():
+            ciphertext += chr((ord(ch) + shift - 65) % 26 + 65)
+        else:
+            ciphertext += chr((ord(ch) + shift - 97) % 26 + 97)
+        i += 1
+
     return ciphertext
 
 
@@ -26,3 +42,7 @@ def decrypt_vigenere(ciphertext, keyword):
     """
     # PUT YOUR CODE HERE
     return plaintext
+
+
+#if __name__ == '__main__':
+#   print(encrypt_vigenere('ATTACKATDAWN', 'LEMON'))
