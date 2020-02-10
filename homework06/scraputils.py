15ea3794833f60cf28e9c42e28dd38a63e3c63cc
h3ic <krelesser@yandex.ru>
2020-02-10 03:03:25 +0300

    extract_next_page removed; get_news working; max_news var added; pep8

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,22 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 from backoff import get
-import re 
+import re
 
 
 def extract_news(parser):
 
     raw_urls = parser.find_all('span', attrs={'class': 'sitebit comhead'})
-    if len(raw_urls) < 30:
-        return None
-    raw_titles = parser.find_all('a', attrs={'class': 'storylink'})
-    raw_points = parser.find_all('span', attrs={'class': 'score'})
-    raw_authors = parser.find_all('a', attrs={'class': 'hnuser'})
-    raw_comments = parser.find_all('a', string=['comments'])
-    subtext = parser.find_all('td', attrs={'class': 'subtext'})
+    max_news = len(raw_urls)
+    raw_titles = parser.find_all('a', attrs={'class': 'storylink'},
+                                 limit=max_news)
+    raw_points = parser.find_all('span', attrs={'class': 'score'},
+                                 limit=max_news)
+    raw_authors = parser.find_all('a', attrs={'class': 'hnuser'},
+                                  limit=max_news)
+    raw_comments = parser.find_all('a', string=['comments'],
+                                   limit=max_news)
+    subtext = parser.find_all('td', attrs={'class': 'subtext'},
+                              limit=max_news)
     news, titles, urls, authors, comments, points = [], [], [], [], [], []
 
-    for i in range(30):
+    for i in range(max_news):
         titles.append(raw_titles[i].text)
         url_text = raw_urls[i].text
         urls.append(re.sub(r'[() ]', '', url_text))
@@ -34,7 +38,7 @@ def extract_news(parser):
         comments.append(comment.replace('\xa0', ''))
 
         item = {}
-        item['title'] = titles[i] 
+        item['title'] = titles[i]
         item['url'] = urls[i]
         item['author'] = authors[i]
         item['comments'] = comments[i]
@@ -43,29 +47,23 @@ def extract_news(parser):
 
     return news
 
-def extract_next_page(parser):
 
-     
-    return next_page
+def get_news(n_pages=1, url='https://news.ycombinator.com/news'):
 
-
-def get_news(url, n_pages=1):
-
-    news = []
+    news_list = []
+    max = n_pages
     while n_pages:
-        print("Collecting data from page: {}".format(url))
+        print('Collecting data from page: {}'.format(url))
         response = get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = BeautifulSoup(response.text, 'html.parser')
         news = extract_news(soup)
-        next_page = extract_next_page(soup)
-        url = "https://news.ycombinator.com/" + next_page
+        # print(news)
+        url = 'https://news.ycombinator.com/news?p=' + str(max - n_pages + 2)
         news_list.extend(news)
         n_pages -= 1
 
     return news_list
 
+
 if __name__ == '__main__':
-    resp = requests.get('https://news.ycombinator.com/')
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    x = extract_news(soup)
-    print(x)
+    print(get_news(5))
